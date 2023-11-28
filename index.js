da4e50ca42214d876dd03e951b581d5e231df4a9
Chigozie Ofodile <chigozie.ofodile@pglnigeria.com>
2023-11-28 16:46:27 +0100

    Update code with Glitch deployment changes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+//var cors = require('cors');
+//app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -70,3 +70,4 @@ app.get('/api/timestamp/:date_string',(req,res)=>{
 var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
